15639bf422c542bf7b27261d8641ae21bcb5e929
Zpiboo <zpiboo@outlook.com>
2025-09-30 23:21:37 +0200

    fix(keybinds): use new KeyBinding.Category instead of a String

--- src/main/java/net/cyvfabric/util/CyvKeybinding.java
+++ src/main/java/net/cyvfabric/util/CyvKeybinding.java
@@ -1,13 +1,15 @@
 package net.cyvfabric.util;
 
+import net.cyvfabric.CyvFabric;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 
 /**CyvFabric custom keybinding*/
 public class CyvKeybinding extends KeyBinding {
     public static final MinecraftClient client = MinecraftClient.getInstance();
-    public static final String KEY_CATEGORY_CYVFABRIC = "key.category.cyvfabric"; //cyvfabric keybinding category
+    public static final KeyBinding.Category KEY_CATEGORY_CYVFABRIC = KeyBinding.Category.create(Identifier.of(CyvFabric.MOD_ID, "cyv_keys")); //cyvfabric keybinding category
 
     public CyvKeybinding(String name, int glfw_key) {
         super(name, InputUtil.Type.KEYSYM, glfw_key, KEY_CATEGORY_CYVFABRIC);
