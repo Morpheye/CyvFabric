1baef380fc974ca5790fa04175a934d62eef17a6
Zpiboo <zpiboo@outlook.com>
2026-04-27 18:13:34 +0200

    refactor: use new LabelFormat class to manage formatting in labels better
    (cherry picked from commit a7651a942da33919f659b0518eef85aed9eb6570)

--- src/main/java/net/cyvfabric/hud/LabelBundle.java
+++ src/main/java/net/cyvfabric/hud/LabelBundle.java
@@ -1,34 +1,10 @@
 package net.cyvfabric.hud;
 
-import net.cyvfabric.config.CyvClientConfig;
 import net.cyvfabric.hud.structure.DraggableHUDElement;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LabelBundle {
-    TextRenderer font = MinecraftClient.getInstance().textRenderer;
-    public List<DraggableHUDElement> labels = new ArrayList<DraggableHUDElement>();
-
-    public int getLabelWidth(String s) {
-        return getLabelWidth(s, false);
-    }
-
-    public int getLabelWidth(String s, boolean angle) {
-        font = MinecraftClient.getInstance().textRenderer;
-
-        StringBuilder str;
-        if (angle) str = new StringBuilder(s + ": 000.");
-        else str = new StringBuilder(s + ": 000000.");
-        for (int i = 0; i< CyvClientConfig.getInt("df",5); i++) str.append("0");
-        if (angle) str.append("\u00B0");
-        return font.getWidth(str.toString());
-    }
-
-    public int getLabelHeight() {
-        return 9;
-    }
-
+    public List<DraggableHUDElement> labels = new ArrayList<>();
 }
--- src/main/java/net/cyvfabric/hud/nonlabels/TogglesprintHUD.java
+++ src/main/java/net/cyvfabric/hud/nonlabels/TogglesprintHUD.java
@@ -1,47 +1,52 @@
 package net.cyvfabric.hud.nonlabels;
 
-import net.cyvfabric.CyvFabric;
 import net.cyvfabric.config.CyvClientColorHelper;
 import net.cyvfabric.hud.structure.DraggableHUDElement;
+import net.cyvfabric.hud.structure.DraggableHUDLabel;
 import net.cyvfabric.hud.structure.ScreenPosition;
 import net.cyvfabric.keybinding.KeybindingTogglesprint;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 
-import java.text.DecimalFormat;
-
 public class TogglesprintHUD extends DraggableHUDElement {
-    public String getName() {return "togglesprintHUD";}
-    public String getDisplayName() {return "Togglesprint HUD";}
-    public boolean enabledByDefault() {return true;}
-    public int getWidth() {
-        TextRenderer font = MinecraftClient.getInstance().textRenderer;
-        return font.getWidth("[Sprint Toggled]");
+    private static final String TEXT = "Sprint Toggled";
+    private static Integer WIDTH = null;
+
+    public TogglesprintHUD() {
+        super("togglesprintHUD", "Togglesprint HUD", true, new ScreenPosition(0, 232));
     }
-    public int getHeight() {return 9;}
-    public ScreenPosition getDefaultPosition() {return new ScreenPosition(0, 232);}
+
+    @Override
+    public int getWidth() {
+        if (WIDTH == null)
+            WIDTH = MinecraftClient.getInstance().textRenderer.getWidth("[" + TEXT + "]");
+
+        return WIDTH;
+    }
+    @Override
+    public int getHeight() {
+        return DraggableHUDLabel.HEIGHT;
+    }
+
+    @Override
     public void render(DrawContext context, ScreenPosition pos) {
         if (!this.isVisible) return;
         if (!KeybindingTogglesprint.sprintToggled) return;
-        long color1 = CyvClientColorHelper.color1.drawColor;
-        long color2 = CyvClientColorHelper.color2.drawColor;
-        TextRenderer font = mc.textRenderer;
-        DecimalFormat df = CyvFabric.df;
-        String p;
-        drawString(context, "[", pos.getAbsoluteX() + 1, pos.getAbsoluteY() + 1, color1);
-        drawString(context, "Sprint Toggled", pos.getAbsoluteX() + 1 + font.getWidth("["), pos.getAbsoluteY() + 1, color2);
-        drawString(context, "]", pos.getAbsoluteX() + 1+ font.getWidth("[Sprint Toggled"), pos.getAbsoluteY() + 1, color1);
 
+        renderText(context, pos);
     }
+    @Override
     public void renderDummy(DrawContext context, ScreenPosition pos) {
-        int d = CyvClientConfig.getInt("df",5);
+        renderText(context, pos);
+    }
+
+    private void renderText(DrawContext context, ScreenPosition pos) {
         long color1 = CyvClientColorHelper.color1.drawColor;
         long color2 = CyvClientColorHelper.color2.drawColor;
         TextRenderer font = mc.textRenderer;
         drawString(context, "[", pos.getAbsoluteX() + 1, pos.getAbsoluteY() + 1, color1);
-        drawString(context, "Sprint Toggled", pos.getAbsoluteX() + 1 + font.getWidth("["), pos.getAbsoluteY() + 1, color2);
-        drawString(context, "]", pos.getAbsoluteX() + 1+ font.getWidth("[Sprint Toggled"), pos.getAbsoluteY() + 1, color1);
+        drawString(context, TEXT, pos.getAbsoluteX() + 1 + font.getWidth("["), pos.getAbsoluteY() + 1, color2);
+        drawString(context, "]", pos.getAbsoluteX() + 1 + font.getWidth("[" + TEXT), pos.getAbsoluteY() + 1, color1);
     }
-
 }
